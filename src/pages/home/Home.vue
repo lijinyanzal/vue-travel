56ce3ffb665721b28b730c524f2ed56819991b67
lijinyan89 <2979096145@qq.com>
2020-07-13 11:56:46 +0800

    project init

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <div>home</div>
+  </div>
+  
+</template>
+
+<script>
+export default {
+  name: 'Home',
+}
+</script>
+
+<style>
+  .home {
+    font-size: 30px;
+  }
+</style>
