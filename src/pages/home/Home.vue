32656f9725a63bb4c4b93ba374dc835cd78bcc3f
lijinyan89 <2979096145@qq.com>
2020-07-16 15:39:15 +0800

    finish pages

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -16,11 +16,13 @@ import HomeIcons from './components/Icons.vue'
 import HomeRecommend from './components/Recommend.vue'
 import HomeWeekend from './components/Weekend.vue'
 import axios from 'axios'
+import { mapState } from 'vuex'
+
 export default {
   name: 'Home',
   data() {
     return {
-
+      lastCity: '',
       swiperList: [],
       iconList: [],
       recommendList: [],
@@ -34,19 +36,29 @@ export default {
     HomeRecommend,
     HomeWeekend,
   },
+  computed: {
+    ...mapState(['city'])
+  },
   mounted() {
+    this.lastCity = this.city
     this.getHomeInfo()
+  },
+  activated() {
+    if (this.lastCity !== this.city) {
+      this.lastCity = this.city
+      this.getHomeInfo()
+    }
+
   },
   methods: {
     getHomeInfo() {
-      axios.get('api/mock/index.json')
+      axios.get('api/mock/index.json?city=' + this.city)
         .then(this.getHomeInfoSucc)
     },
     getHomeInfoSucc(res) {
       res = res.data
       if (res.ret && res.data) {
         const data = res.data
-        this.city = data.city
         this.swiperList = data.swiperList
         this.iconList = data.iconList
         this.recommendList = data.recommendList
